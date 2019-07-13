61d6b194f1e387123f0fe577aa84aeaeb67f7d2e
Sean Anderson <sean@seananderson.ca>
2019-07-12 18:05:43 -0700

    Fix R CMD check issues

--- R/plot.R
+++ R/plot.R
@@ -13,21 +13,22 @@
 #' @param ref_lines_label_spoke A vector of reference label locations (spoke number). A value of
 #'  zero means that the label will not be printed
 #' @param diff_lty_by_name This is an optional string which will be used to [grep()] the names
-#'  of the values given in column grp_col. If they match, the diff_lty value will be used for
+#'  of the values given in column grp_col. If they match, the `diff_lty` value will be used for
 #'  line type for those lines
-#' @param diff_lty Line type to use for grp_col values that diff_lty_by_name matches with [grep()]
+#' @param diff_lty Line type to use for `grp_col` values that `diff_lty_by_name` matches with [grep()]
 #' @param palette As defined in [ggplot2::scale_color_brewer()]
 #' @param show_legend Show legend?
 #' @param leg_main_title Main legend title
 #' @param leg_lty_title Linetype legend title
 #'
-#' @importFrom ggplot2 ggplot aes geom_path geom_segment geom_text coord_equal
+#' @importFrom ggplot2 ggplot aes_string geom_path geom_segment geom_text coord_equal
 #' @importFrom ggplot2 labs theme scale_color_brewer element_blank
 #'
 #' @importFrom tidyr spread
 #' @importFrom dplyr mutate select left_join
 #' @importFrom magrittr "%>%"
 #' @importFrom utils head tail
+#' @importFrom rlang .data
 #' @export
 #'
 #' @examples
@@ -83,17 +84,17 @@ spider_web <- function(df,
   if (!any("data.frame" %in% class(df)) || length(df) < 3 || nrow(df) < 1) {
     stop("Argument 'df' must be a data frame with at least one row and three columns")
   }
-  if (class(grp_col) != "character" | length(grp_col) != 1 | !grp_col %in% names(df)) {
+  if (class(grp_col) != "character" || length(grp_col) != 1 || !grp_col %in% names(df)) {
     stop("grp_col is either of the wrong type or is not a column of df",
       call. = FALSE
     )
   }
-  if (class(spk_col) != "character" | length(spk_col) != 1 | !spk_col %in% names(df)) {
+  if (class(spk_col) != "character" || length(spk_col) != 1 || !spk_col %in% names(df)) {
     stop("spk_col is either of the wrong type or is not a column of df",
       call. = FALSE
     )
   }
-  if (class(value_col) != "character" | length(value_col) != 1 | !value_col %in% names(df)) {
+  if (class(value_col) != "character" || length(value_col) != 1 || !value_col %in% names(df)) {
     stop("value_col is either of the wrong type or is not a column of df",
       call. = FALSE
     )
@@ -115,7 +116,7 @@ spider_web <- function(df,
       call. = FALSE
     )
   }
-  if (class(ref_lines_color) != "character" & class(ref_lines_color) != "numeric") {
+  if (class(ref_lines_color) != "character" && class(ref_lines_color) != "numeric") {
     stop("ref_lines_color must be a vector of character values (color names) or numeric values representing colors",
       call. = FALSE
     )
@@ -125,25 +126,25 @@ spider_web <- function(df,
       call. = FALSE
     )
   }
-  if (class(ref_label_color) != "character" & class(ref_label_color) != "numeric") {
+  if (class(ref_label_color) != "character" && class(ref_label_color) != "numeric") {
     stop("ref_label_color must be a vector of character values (color names) or numeric values representing colors",
       call. = FALSE
     )
   }
-  if (class(ref_lines_label_spoke) != "integer" & class(ref_lines_label_spoke) != "numeric") {
+  if (class(ref_lines_label_spoke) != "integer" && class(ref_lines_label_spoke) != "numeric") {
     stop("ref_lines_label_spoke must be a vector of numeric values",
       call. = FALSE
     )
   }
-  if (class(show_legend) != "logical" | length(show_legend) > 1) {
+  if (class(show_legend) != "logical" || length(show_legend) > 1) {
     stop("show_legend must be a single TRUE or FALSE value",
       call. = FALSE
     )
   }
 
-  if (length(ref_lines_val) != length(ref_lines_color) |
-    length(ref_lines_color) != length(ref_lines_type) |
-    length(ref_lines_type) != length(ref_label_color) |
+  if (length(ref_lines_val) != length(ref_lines_color) ||
+    length(ref_lines_color) != length(ref_lines_type) ||
+    length(ref_lines_type) != length(ref_label_color) ||
     length(ref_label_color) != length(ref_lines_label_spoke)) {
     stop("ref_lines_val, ref_lines_color, ref_lines_type, ref_label_color, and ref_lines_label_spoke must all be the same length",
       call. = FALSE
@@ -162,7 +163,7 @@ spider_web <- function(df,
   ds <- tidyr::spread(df, key = spk_col, value = value_col)
   spider_data <- calc_web(ds)
   ref_lines_attr <- data.frame(
-    line_num = 1:length(ref_lines_val),
+    line_num = seq_len(length(ref_lines_val)),
     x = ref_lines_val,
     color = ref_lines_color,
     type = ref_lines_type
@@ -172,11 +173,11 @@ spider_web <- function(df,
   ref_lines_data <- data.frame()
   for (i in seq_along(ref_lines_val)) {
     k <- rbind(spokes, spokes[1, ]) %>%
-      dplyr::select(-c(x, y, spk_nms)) %>%
+      dplyr::select(-c(.data$x, .data$y, .data$spk_nms)) %>%
       dplyr::mutate(
         line_num = ref_lines_attr[i, ]$line_num,
-        x = xend * ref_lines_attr[i, ]$x,
-        y = yend * ref_lines_attr[i, ]$x,
+        x = .data$xend * ref_lines_attr[i, ]$x,
+        y = .data$yend * ref_lines_attr[i, ]$x,
         color = ref_lines_attr[i, ]$color,
         lty = ref_lines_attr[i, ]$type
       )
@@ -188,7 +189,7 @@ spider_web <- function(df,
   if (length(ref_lines_to_label)) {
     label_data <- data.frame(x = 0, y = ref_lines_to_label)
     label_data <- label_data %>% dplyr::mutate(
-      label = y,
+      label = .data$y,
       color = ref_label_color
     )
     label_spokes <- spokes[ref_lines_label_spoke, ]
@@ -213,48 +214,48 @@ spider_web <- function(df,
   spokes$spk_nms <- c(tail(spokes$spk_nms, -1), head(spokes$spk_nms, 1))
 
   g <- spider_data %>%
-    ggplot(aes(x = x, y = y)) +
+    ggplot(aes_string(x = "x", y = "y")) +
     geom_segment(
       data = spokes,
-      aes(
-        x = x,
-        y = y,
-        xend = xend,
-        yend = yend
+      aes_string(
+        x = "x",
+        y = "y",
+        xend = "xend",
+        yend = "yend"
       ),
       color = spoke_color,
       lty = spoke_lty
     ) +
     geom_path(
       data = ref_lines_data,
-      aes(x,
-        y,
-        group = line_num
+      aes_string("x",
+        "y",
+        group = "line_num"
       ),
       color = ref_lines_data$color,
       lty = ref_lines_data$lty,
       inherit.aes = FALSE
     ) +
-    geom_path(aes(
-      color = as.factor(group),
-      linetype = as.factor(lty)
+    geom_path(aes_string(
+      color = "as.factor(group)",
+      linetype = "as.factor(lty)"
     ),
     lwd = 0.8
     ) +
     coord_equal() +
     geom_text(
       data = spokes,
-      aes(
-        x = xend * 1.1,
-        y = yend * 1.1,
-        label = spk_nms
+      aes_string(
+        x = "xend * 1.1",
+        y = "yend * 1.1",
+        label = "spk_nms"
       ),
       colour = "grey30"
     ) +
-    gfplot::theme_pbs() +
-    scale_color_brewer(name = leg_main_title, palette = palette, guide = "legend") +
-    scale_linetype_discrete(guide = FALSE) +
-    theme(
+    ggplot2::scale_color_brewer(name = leg_main_title, palette = palette, guide = "legend") +
+    ggplot2::scale_linetype_discrete(guide = FALSE) +
+    ggplot2::theme_light() +
+    ggplot2::theme(
       axis.line = element_blank(),
       axis.text.x = element_blank(),
       axis.text.y = element_blank(),
@@ -267,17 +268,17 @@ spider_web <- function(df,
       panel.grid.minor = element_blank(),
       plot.background = element_blank()
     ) +
-    theme(legend.position = "right")
+    ggplot2::theme(legend.position = "right")
 
   if (length(ref_lines_to_label)) {
     g <- g + geom_text(
       data = label_data,
-      aes(x = x, y = y, label = label), color = ref_label_color,
+      aes_string(x = "x", y = "y", label = "label"), color = ref_label_color,
       nudge_y = 0.04, hjust = 0, nudge_x = 0.01
     )
   }
   if (!show_legend) {
-    g <- g + theme(legend.position = "none")
+    g <- g + ggplot2::theme(legend.position = "none")
   }
   g <- g + labs(linetype = leg_lty_title)
 
